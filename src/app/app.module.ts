8546a67eb72c4cb164644392d26a604a0e6ff9c5
Ahmed Mujtaba Khan <wdesigns15@tibolli.lan>
2019-06-20 00:53:13 +0500

    new component created for table generation using angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MakingTableWithEditableDeleteableRowsComponent } from './making-table-with-editable-deleteable-rows/making-table-with-editable-deleteable-rows.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MakingTableWithEditableDeleteableRowsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/making-table-with-editable-deleteable-rows/making-table-with-editable-deleteable-rows.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-making-table-with-editable-deleteable-rows',
+  templateUrl: './making-table-with-editable-deleteable-rows.component.html',
+  styleUrls: ['./making-table-with-editable-deleteable-rows.component.css']
+})
+export class MakingTableWithEditableDeleteableRowsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
