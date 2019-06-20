e7b4e00cb7792b10fe024a676bdf7a700011e5e6
Ahmed Mujtaba Khan <wdesigns15@tibolli.lan>
2019-06-20 19:43:10 +0500

    tried to add font-awesome but failed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms'; //angular forms
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; //bootstrap
+import { AngularFontAwesomeModule } from 'angular-font-awesome'; //fontawesome
 import { AppComponent } from './app.component';
 import { EditableTableComponent } from './editable-table/editable-table.component';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'about', component: EditableTableComponent },
+  { path: 'editable-table', component: EditableTableComponent },
   { path: '', component: HomeComponent },
   { path: '**', component: HomeComponent },
 ]
@@ -21,7 +22,8 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    NgbModule,
+    NgbModule, //bootstrap after here, it was @imported to style.css
+    AngularFontAwesomeModule, //fontawesome
     RouterModule.forRoot(routes),
     FormsModule,
   ],
