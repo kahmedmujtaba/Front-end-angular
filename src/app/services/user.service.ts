727f98e4d3f1d49c3eac385b19cbf9e5b64081da
Ahmed Mujtaba Khan <wdesigns15@tibolli.lan>
2019-06-21 20:46:17 +0500

    created HttpClient service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,13 +9,13 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getUserData() : Observable<any>{
+  getUserData() : Observable<any> {
     // return [
     //   {name:"Ahmed",age:"31", email:"kahmedm@gmail.com",mstatus:"married"},
     //   {name:"Mujtaba",age:"32", email:"kamujtaba@gmail.com",mstatus:"married"},
     //   {name:"Ahmed Mujtaba",age:"31", email:"kahmedmujtaba@gmail.com",mstatus:"married"},
-    //   {name:"foo",age:"56", email:"foo@gmail.com",mstatus:"foo"},
+    //   {name:"foo",age:"56", email:"foo@gmail.com",mstatus:['married','un married']},
     //   ];
-    return this.http.get('https://www.example.com');
+    return this.http.get('http://www.mocky.io/v2/5d0cfad03500006500b89c0c');
   }
 }
